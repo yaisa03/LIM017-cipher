b80a212f8c6f57b85938fbd6b9178d0c3a2f3828
yaisa03 <yaisamichellea@gmail.com>
2022-02-22 14:40:15 -0500

    cifra y descifra algunos caracteres especiales

--- src/cipher.js
+++ src/cipher.js
@@ -18,9 +18,20 @@ const cipher = {
       } else if (stringAscii >= 97 && stringAscii <= 122) {
         let textOffset = ((stringAscii - 97 + parseInt(offset)) % 26) + 97;
         textCod += String.fromCharCode(textOffset);
-      } else {
-        textCod += String.fromCharCode(stringAscii)
+        
+        /*caracteres especiales*/
+      } else if(stringAscii >= 32 && stringAscii <= 64){
+        let textOffset = ((stringAscii - 32 + parseInt(offset)) % 33) + 32; 
+        textCod += String.fromCharCode(textOffset);
+        
+      }else if(stringAscii >= 91  && stringAscii <= 96){
+        let textOffset = ((stringAscii - 91 + parseInt(offset)) % 6) + 91;
+        textCod += String.fromCharCode(textOffset);
       }
+      /*else if(stringAscii >= 123 && stringAscii <= 254){
+        let textOffset = ((stringAscii - 123 + parseInt(offset)) % 132) + 123;
+        textCod += String.fromCharCode(textOffset);
+      }*/
     }
     return textCod;
   },
@@ -40,11 +51,21 @@ const cipher = {
         textDeCod += String.fromCharCode(newtext);
 
       } else if (stringAscii >= 97 && stringAscii <= 122) {
-        let newtext = ((stringAscii - 45 - parseInt(offset)) % 26) + 97; //97-(26*2)
+        let newtext = ((stringAscii - 45 - parseInt(offset)) % 26) + 97; //(26*2)-97
         textDeCod += String.fromCharCode(newtext);
-      } else {
-        textDeCod += String.fromCharCode(stringAscii)
+
+      } else if(stringAscii >= 32 && stringAscii <= 64){
+        let newtext = ((stringAscii + 1 - parseInt(offset)) % 33) + 32; //33-32
+        textDeCod += String.fromCharCode(newtext);
+
+      }else if(stringAscii >= 91  && stringAscii <= 96){
+        let newtext = ((stringAscii + 5 - parseInt(offset)) % 6) + 91;
+        textDeCod += String.fromCharCode(newtext);
       }
+      /*else if(stringAscii >= 123  && stringAscii <= 255){
+        let newtext = ((stringAscii - 45 - parseInt(offset)) % 26) + 123; //97-(26*2)
+        textDeCod += String.fromCharCode(newtext);
+      }*/
     }
     return textDeCod;
   }
